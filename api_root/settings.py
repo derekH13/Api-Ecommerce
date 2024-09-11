5b871ce0e8b2080e32f83b348398b877ff26baa9
derekH13 <168042164+derekH13@users.noreply.github.com>
2024-09-11 12:42:37 -0300

    Update settings.py

--- api_root/settings.py
+++ api_root/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_ORIGINS = [
     'http://localhost:8000',
